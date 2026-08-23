212f7808045590c8f4c017412fa33b17c46cc1ba
aryanrai1503-cloud <aryan.rai.1503@gmail.com>
2026-08-23 21:32:26 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int l=0,r=n-1;
+        int lmax=0,rmax=0,totvol=0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(lmax>height[l]){
+                    totvol+=lmax-height[l];
+                }else{
+                    lmax=height[l];
+                }
+                l++;
+            }else{
+                if(rmax>height[r]){
+                    totvol+=rmax-height[r];
+                }
+                else{
+                    rmax=height[r];
+                }
+                r--;
+            }
+        }
+        return totvol;
+    }
+};
